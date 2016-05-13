b461f11f5defe0eaff410f95fc2a91cd2e85bad9
Claudio Heckler <claudio.heckler@hp.com>
2016-05-13 12:40:43 -0300

    renamed 'timeout' to 'latency' in the dependency

--- MyExternalDependency/src/main/java/com/example/MyExternalDependencyApplication.java
+++ MyExternalDependency/src/main/java/com/example/MyExternalDependencyApplication.java
@@ -12,16 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class MyExternalDependencyApplication {
 
-	static int timeout = 400;
+	static int latency = 400;
 	static int errorRate = 10;
 
 	@RequestMapping(value = "/configure")
 	public String configure(
-			@RequestParam(required = false, value = "timeout", defaultValue = "0") int timeout_value,
+			@RequestParam(required = false, value = "latency", defaultValue = "0") int latency_value,
 			@RequestParam(required = false, value = "error", defaultValue = "0") int error_value) {
 
-		if (timeout_value > 0) {
-			timeout = timeout_value;
+		if (latency_value > 0) {
+			latency = latency_value;
 		}
 
 		if ( (error_value >= 0) && (error_value<=100) ) {
@@ -29,19 +29,19 @@ public class MyExternalDependencyApplication {
 		}
 
 		return ("Current configuration is:\n\n" +
-				"timeout:\t" + timeout + "ms\n" +
-				"error:  \t" + errorRate + "%\n\n");
+				"latency:\t" + latency + "ms\n" +
+				"errors: \t" + errorRate + "%\n\n");
 	}
 
 	@RequestMapping(value = "/external")
 	public String external() throws Exception {
-		Thread.sleep(timeout/2);
+		Thread.sleep(latency/2);
 
 		if ( (Math.random()*100) < errorRate) {
 			throw new Exception("General service error");
 		}
 
-		Thread.sleep(timeout/2);
+		Thread.sleep(latency/2);
 
 		return "Message from external app";
 	}

525c47d083b0cf9f239517d57dc9a10bb31ef6dd
tiago.melo <tiago.melo@hp.com>
2016-05-16 15:25:39 -0300

    Add custom configuration to spring approach.

--- MySystem/src/main/java/com/example/services/ExternalSafeRequestorService.java
+++ MySystem/src/main/java/com/example/services/ExternalSafeRequestorService.java
@@ -8,12 +8,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class ExternalSafeRequestorService {
 	
 	
-	@HystrixCommand(fallbackMethod = "fallback")
+	@HystrixCommand(fallbackMethod = "fallback", commandProperties = {
+			@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "10")
+			})
 	public ResponseEntity<String> safe(){
 		RestTemplate restTempalte = new RestTemplate();
 		URI uri = URI.create("http://localhost:9090/external");
